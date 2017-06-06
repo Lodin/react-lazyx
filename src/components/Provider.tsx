49626c560c6b0cd181ef14fd3741af6653f77802
Vlad Rindevich <rindevich.vs@gmail.com>
2017-06-06 21:50:29 +0300

    refactor(components): make Provider component createable with function to simplify specifying NODE_ENV
    * Move Provider creation to function
    * Improve Provider typings

--- src/components/Provider.tsx
+++ src/components/Provider.tsx
@@ -1,49 +1,48 @@
 import {Store} from 'lazyx';
 import * as PropTypes from 'prop-types';
-import {ChildContextProvider, Children, Component, ReactChildren, ReactElement} from 'react';
+import {ChildContextProvider, Children, Component, ReactElement} from 'react';
 import {storeShape} from '../utils/propTypes';
 import {StoreContainer} from '../utils/types';
 
-export type ProviderProps = StoreContainer & {
-  children: ReactChildren;
-};
+export function createProvider(env: string): any {
+  class Provider
+    extends Component<StoreContainer, null>
+    implements ChildContextProvider<StoreContainer> {
 
-type ProviderState = null;
+    public static propTypes = {
+      children: PropTypes.element.isRequired,
+      store: storeShape.isRequired,
+    };
 
-export default class Provider
-  extends Component<ProviderProps, ProviderState>
-  implements ChildContextProvider<StoreContainer> {
+    public static childContextTypes = {
+      store: storeShape.isRequired,
+    };
 
-  public static propTypes = {
-    children: PropTypes.element.isRequired,
-    store: storeShape.isRequired,
-  };
+    private store: Store;
 
-  public static childContextTypes = {
-    store: storeShape.isRequired,
-  };
-
-  public props: ProviderProps;
-  private store: Store;
-
-  public constructor(props: ProviderProps, context?: any) {
-    super(props, context);
-    this.store = props.store;
-  }
-
-  public getChildContext(): StoreContainer {
-    return {store: this.store};
-  }
-
-  public render(): ReactElement<any> {
-    return Children.only(this.props.children);
-  }
-}
-
-if (process.env.NODE_ENV !== 'production') {
-  (Provider as any).prototype.componentWillReceiveProps = function (nextProps: any): void {
-    if (this.store !== nextProps.store) {
-      throw new Error('<Provider> does not support changing `store` on the fly.');
+    public constructor(props: StoreContainer, context?: any) {
+      super(props, context);
+      this.store = props.store;
     }
-  };
+
+    public getChildContext(): StoreContainer {
+      return {store: this.store};
+    }
+
+    public render(): ReactElement<any> {
+      return Children.only(this.props.children);
+    }
+  }
+
+  if (env !== 'production') {
+    (Provider as any).prototype.componentWillReceiveProps = function (nextProps: any): void {
+      if (this.store !== nextProps.store) {
+        console.error('<Provider> does not support changing `store` on the fly.');
+      }
+    };
+  }
+
+  return Provider;
 }
+
+export default createProvider(process.env.NODE_ENV);
