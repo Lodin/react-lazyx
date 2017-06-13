dc95d815e1bba237b49c1ff3da228837cec7c4d4
Vlad Rindevich <rindevich.vs@gmail.com>
2017-06-13 12:53:35 +0300

    fix: add "connect" and "connectDynamically" to index export.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,3 @@
 export {default as Provider} from './components/Provider';
+export {default as connect} from './connectors/connect';
+export {default as connectDynamically} from './connectors/connectDynamically';
